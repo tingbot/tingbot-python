7e87c3913db4d29892682d03a7b4626d9d4c1e01
Joe Rickerby <joerick@mac.com>
2015-10-30 19:22:50 +0000

    Update copyright

--- docs/conf.py
+++ docs/conf.py
@@ -56,7 +56,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Tingbot'
-copyright = u'2015, Joe Rickerby'
+copyright = u'2015, Tingbot Ltd.'
 
 # The version info for the project you're documenting, acts as replacement
 # for |version| and |release|, also used in various other places throughout

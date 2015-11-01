594a9f1bd5eec62a55fb90115efef4b1705b7582
Joe Rickerby <joerick@mac.com>
2015-11-01 14:59:20 +0000

    Remove template stuff from setup.py

--- setup.py
+++ setup.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-
 try:
     from setuptools import setup
 except ImportError:
@@ -12,10 +11,6 @@ with open('README.rst') as readme_file:
 
 requirements = [
     # TODO: put package requirements here
-]
-
-test_requirements = [
-    # TODO: put package test requirements here
 ]
 
 setup(
@@ -33,21 +28,13 @@ setup(
                  'tingbot'},
     include_package_data=True,
     install_requires=requirements,
-    license="ISCL",
+    license="BSD",
     zip_safe=False,
     keywords='tingbot',
     classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: ISC License (ISCL)',
         'Natural Language :: English',
         "Programming Language :: Python :: 2",
-        'Programming Language :: Python :: 2.6',
         'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
     ],
-    test_suite='tests',
-    tests_require=test_requirements
 )

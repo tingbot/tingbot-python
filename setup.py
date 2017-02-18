556194cd7b6840d3990614bf80b515784b1c855b
Joe Rickerby <joerick@mac.com>
2017-02-18 19:43:02 +0000

    Bump version

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ if 'arm' in platform.machine():
 
 setup(
     name='tingbot-python',
-    version='1.0.2',
+    version='1.1.0',
     description="Python APIs to write apps for Tingbot",
     long_description=readme,
     author="Joe Rickerby",
